6759dbd77edf705e3967d0e1aa2d077f3c999002
Pavel068 <Pavel068>
2022-06-04 15:19:13 +0300

    Charts added

--- views/analytics/index.php
+++ views/analytics/index.php
@@ -14,6 +14,69 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row mt-2">
         <div class="col-lg-6">
             <div class="card">
+                <div class="card-header">Типы приробов</div>
+                <div class="card-body">
+                    <?=
+                    ChartJs::widget([
+                        'type' => 'doughnut',
+                        'id' => uniqid(true),
+                        'options' => [
+                            'responsive' => true,
+                            'maintainAspectRatio' => false,
+                        ],
+                        'data' => [
+                            'labels' => ['Счетчик газа бытовой', 'Линейка деревянная'],
+                            'datasets' => [
+                                [
+                                    'data' => [6600, 1000254],
+                                    'backgroundColor' => [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(0, 205, 86)'
+                                    ]
+                                ]
+                            ]
+                        ],
+                    ]);
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-header">По странам</div>
+                <div class="card-body">
+                    <?=
+                    ChartJs::widget([
+                        'type' => 'doughnut',
+                        'id' => uniqid(true),
+                        'options' => [
+                            'responsive' => true,
+                            'maintainAspectRatio' => false,
+                        ],
+                        'data' => [
+                            'labels' => ['Россия', 'США', 'Германия', 'Китай'],
+                            'datasets' => [
+                                [
+                                    'data' => [55, 2, 25, 50],
+                                    'backgroundColor' => [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(255, 205, 86)',
+                                        'rgb(0, 205, 86)'
+                                    ]
+                                ]
+                            ]
+                        ],
+                    ]);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-lg-6">
+            <div class="card">
                 <div class="card-header">По странам</div>
                 <div class="card-body">
                     <?=
